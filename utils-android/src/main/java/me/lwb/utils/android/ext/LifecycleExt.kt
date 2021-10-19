cea3d97cf00efc4a2803370cf52ef45a6353ea9c
luowenbin <ve3344@qq.com>
2021-10-19 15:55:19 +0800

    feat(*): 去除 LifecycleOwnerContextExt.kt,添加 ResourcesExt 拓展

--- utils-android/src/main/java/me/lwb/utils/android/ext/LifecycleExt.kt
+++ utils-android/src/main/java/me/lwb/utils/android/ext/LifecycleExt.kt
@@ -3,75 +3,44 @@ package me.lwb.utils.android.ext
 import androidx.lifecycle.*
 
 /**
- * 监听LifecycleOwner 生命周期
- * @param lifecycleOwner lifecycleOwner
- * @param targetEvent 目标事件
- * @param block 处理内容，返回true结束监听
- */
-private class LifecycleListener(
-    lifecycleOwner: LifecycleOwner,
-    private val targetEvent: Lifecycle.Event,
-    block: () -> Boolean
-) : LifecycleEventObserver {
-
-    private var lifecycleOwner: LifecycleOwner? = lifecycleOwner
-
-    private var block: (() -> Boolean?)? = block
-
-
-    init {
-        lifecycleOwner.lifecycle.addObserver(this)
-    }
-
-    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
-        if (targetEvent == event) {
-            if (block?.invoke() == true) {
-                destroy()
-            }
-        }
-        if (event == Lifecycle.Event.ON_DESTROY) {
-            destroy()
-        }
-    }
-
-    /**
-     * 清理引用
-     */
-    private fun destroy() {
-        if (lifecycleOwner == null) {
-            return
-        }
-        lifecycleOwner?.lifecycle?.removeObserver(this)
-        lifecycleOwner = null
-        block = null
-    }
-}
-
-/**
  * 当到达指定生命周期状态时执行
- * @param event 生命周期事件
+ * @param targetEvent 生命周期事件
  * @param block 处理内容
  */
-fun LifecycleOwner.doOnLifecycle(event: Lifecycle.Event, block: () -> Boolean) {
-    LifecycleListener(this, event, block)
+fun LifecycleOwner.doOnLifecycle(targetEvent: Lifecycle.Event, block: () -> Unit) {
+    lifecycle.addObserver(object : LifecycleEventObserver {
+        override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+            if (targetEvent == event) {
+                block()
+            }
+        }
+    })
 }
 
 
 /**
  * 当到达指定生命周期状态时执行（仅一次）
- * @param event 生命周期事件
+ * @param targetEvent 生命周期事件
  * @param block 处理内容
  */
-inline fun LifecycleOwner.doOnLifecycleOnce(event: Lifecycle.Event, crossinline block: () -> Unit) {
-    doOnLifecycle(event) { block(); true }
+fun LifecycleOwner.doOnLifecycleOnce(targetEvent: Lifecycle.Event, block: () -> Unit) {
+    lifecycle.addObserver(object : LifecycleEventObserver {
+        override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+            if (targetEvent == event) {
+                block()
+                lifecycle.removeObserver(this)
+            }
+        }
+    })
 }
 
 /**
  * 当到达生命周期onDestroy执行（仅一次）
  * @param block 处理内容
  */
-inline fun LifecycleOwner.doOnDestroyOnce(crossinline block: () -> Unit) =
+fun LifecycleOwner.doOnDestroyOnce(block: () -> Unit) =
     doOnLifecycleOnce(Lifecycle.Event.ON_DESTROY, block)
+
 
 
 
--- utils-android/src/main/java/me/lwb/utils/android/ext/ResourcesExt.kt
+++ utils-android/src/main/java/me/lwb/utils/android/ext/ResourcesExt.kt
@@ -1,6 +1,18 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
 package me.lwb.utils.android.ext
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.content.res.Resources
+import android.graphics.Typeface
+import android.graphics.drawable.Drawable
+import android.graphics.fonts.Font
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.annotation.FontRes
+import androidx.core.content.res.ResourcesCompat
+
 /**
  * 获取尺寸
  * @param idName id名
@@ -8,4 +20,25 @@ import android.content.res.Resources
  */
 fun Resources.getAndroidDimensionPx(idName: String): Int {
     return getDimensionPixelSize(getIdentifier(idName, "dimen", "android"))
+}
+
+
+inline fun Context.getColorCompat(@ColorRes resId: Int): Int {
+    return ResourcesCompat.getColor(resources, resId, theme)
+}
+
+inline fun Context.getColorStateListCompat(@ColorRes resId: Int): ColorStateList? {
+    return ResourcesCompat.getColorStateList(resources, resId, theme)
+}
+
+inline fun Context.getDrawableCompat(@DrawableRes resId: Int): Drawable? {
+    return ResourcesCompat.getDrawable(resources, resId, theme)
+}
+
+inline fun Context.getDrawableForDensityCompat(@DrawableRes resId: Int, density: Int): Drawable? {
+    return ResourcesCompat.getDrawableForDensity(resources, resId, density, theme)
+}
+
+inline fun Context.getFontCompat(@FontRes resId: Int): Typeface? {
+    return ResourcesCompat.getFont(this, resId)
 }
